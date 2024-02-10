dd0f4bc704d8e109e2063245bb02d2330210050d
Jhayrolandero <xjaylandero@gmail.com>
2024-02-10 17:48:33 +0800

    Adding post

--- index.php
+++ index.php
@@ -2,37 +2,21 @@
 
 require "./router/Router.php";
 
-$router = new Router();
 
 if (isset($_REQUEST["request"])) {
     $request = explode('/', $_REQUEST["request"]);
 } else {
     http_response_code(404);
 }
+$router = new Router($_SERVER["REQUEST_METHOD"], $request[0]);
 
 // ("path", "callback")
 $router->get("user", "UserController@index");
+$router->post("user", "UserController@store");
+
+$router->run();
 
 
-$router->run($_SERVER["REQUEST_METHOD"], $request[0]);
-
-
-
-
-
-// switch ($_SERVER["REQUEST_METHOD"]) {
-
-//     case "GET":
-//         switch ($request[0]) {
-//             case "user":
-//                 echo json_encode($user->index());
-//                 break;
-//         }
 
 //     case "POST":
 //         $data = json_decode(file_get_contents("php://input"));
-
-//     default:
-//         http_response_code(404);
-//         break;
-// }
